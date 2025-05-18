bd62b31e37e72403a15ae57df29ff4d690a4d4c2
AQUELEBIXO\Bruno <leorecoa1@hotmail.com>
2025-05-18 02:54:05 -0300

    fix: remove pasta android duplicada para resolver conflito de elemento raiz

--- android/app/src/main/java/com/bruno/callshield/CallSettingsPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.bruno.callshield;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.annotation.CapacitorPlugin;
-
-@CapacitorPlugin(name = "CallSettings")
-public class CallSettingsPlugin extends Plugin {
-}
